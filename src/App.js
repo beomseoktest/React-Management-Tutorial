a9449454825030650fa30bad21a07bc51da9de79
beomseoktest <ggeggebbo@naver.com>
2020-03-20 10:44:20 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,52 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
+
+const params = [{
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/any/1',
+  'name' : '이범석',
+  'birthday' : '861012',
+  'gender' : '남자',
+  'job' : '대학생',
+},{
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/any/2',
+  'name' : '홍길동',
+  'birthday' : '301212',
+  'gender' : '남자',
+  'job' : '도적',
+},{
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/any/3',
+  'name' : '나동빈',
+  'birthday' : '961222',
+  'gender' : '남자',
+  'job' : '프로그래머',
+}]
 
 class App extends Component {
   render(){
     return (
-      <div className="gray-background">
-        <img src={logo} lat="logo" />
-        <h2>Let's develop management system!</h2>
+      <div>
+      {
+        params.map(c => {
+          return(
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+              />
+          )
+        })
+      }
       </div>
-    )
+    );
   }
 }
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Hello React Project!
-//         </p>
-//         {/* <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a> */}
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
